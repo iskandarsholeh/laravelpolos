94f6825c677942bcb93e94d407d2ddfb7f0b9e05
Iskandar Sholeh <79823292+iskandarsholeh@users.noreply.github.com>
2021-06-25 21:39:08 +0700

    larave

---
+++ database/migrations/2021_06_25_141344_create_pembayaran_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembayaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id') ->references('id') ->on('orders') ->onDelete('cascade') ->onUpdate('cascade');
+            $table->string('bukti');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+}
---
+++ database/migrations/2021_06_25_143039_create_materi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMateriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kelas_id') ->references('id') ->on('kelas') ->onDelete('cascade') ->onUpdate('cascade');
+            $table->string('nama_materi');
+            $table->string('foto');
+            $table->string('keterangan');
+            $table->string('video');
+            $table->string('modul');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materi');
+    }
+}
